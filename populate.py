18afaac86cbecd7641ba19068fe6dece8ff74fc5
malcubierre <malcubierre@gfi.es>
2019-10-10 09:27:06 +0200

    Include populate records

---
+++ populate.py
@@ -0,0 +1,57 @@
+from app import flask_app
+from sqlalchemy.sql import text
+from sqlalchemy import create_engine
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+
+with engine.connect() as con:
+
+    data = ( { "id": 1, "username": "username 1", "email": "user1@email.com", "password_hash": "pass_bla_bla" },
+             { "id": 2, "username": "username 2", "email": "user2@email.com", "password_hash": "pass_bla_bla" },
+             { "id": 3, "username": "username 3", "email": "user3@email.com", "password_hash": "pass_bla_bla" },
+             { "id": 4, "username": "username 4", "email": "user4@email.com", "password_hash": "pass_bla_bla" },
+             { "id": 5, "username": "username 5", "email": "user5@email.com", "password_hash": "pass_bla_bla" },
+             { "id": 6, "username": "username 6", "email": "user6@email.com", "password_hash": "pass_bla_bla" },
+             { "id": 7, "username": "username 7", "email": "user7@email.com", "password_hash": "pass_bla_bla" },
+             { "id": 8, "username": "username 8", "email": "user8@email.com", "password_hash": "pass_bla_bla" },
+             { "id": 9, "username": "username 9", "email": "user9@email.com", "password_hash": "pass_bla_bla" },
+             { "id": 10, "username": "username 10", "email": "user10@email.com", "password_hash": "pass_bla_bla" },
+             { "id": 11, "username": "username 11", "email": "user11@email.com", "password_hash": "pass_bla_bla" },
+    )
+
+    statement = text("""INSERT INTO user(id, username, email, password_hash) VALUES(:id, :username, :email, :password_hash)""")
+
+    for line in data:
+        con.execute(statement, **line)
+
+    data = ( { "id": 1, "body": "Fisrt Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 2, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 3, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 4, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 5, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 6, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 7, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 8, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 9, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 10, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 11, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 12, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 13, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 14, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 15, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 16, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 17, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 18, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 19, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+             { "id": 20, "body": "Another Post", "timestamp": "2019-10-10 06:41:45.189000", "user_id": "1" },
+    )
+
+    statement = text("""INSERT INTO post(id, body, timestamp) VALUES(:id, :body, :timestamp)""")
+
+    for line in data:
+        con.execute(statement, **line)
--- readme.txt
+++ readme.txt
@@ -1,6 +1,14 @@
 follows instructions from https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
 
+First time to create database scripts
+
+$ flask db init
+
 On every database changes (models.py):
 
 $ flask db migrate -m "change"
 $ flask db upgrade
+
+You can populate some records to play
+
+$ python populate.py
